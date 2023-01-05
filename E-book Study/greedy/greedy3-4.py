30ea6a765d5976be27408565edd404c06e3d1d96
csyeob <csyeob@gmail.com>
2023-01-05 17:38:30 +0900

    E-book greedy 3-4

--- E-book Study/greedy/greedy3-4.py
+++ E-book Study/greedy/greedy3-4.py
@@ -1,13 +1,14 @@
-import time
+
 n, k = map(int, input().split())
 count = 0
-start_time = time.time()
-num = n - 1
-count += 1
-while k <= num:
-        num = num // k
-        count += 1
-
+while k <= n:
+        if(n % k == 0):
+            n = n // k
+            count += 1
+        else:
+            n = n -1
+            count += 1
+while n > 1:
+    n = n-1
+    count += 1
 print(count)
-end_time = time.time()
-print("time: ", end_time-start_time)
