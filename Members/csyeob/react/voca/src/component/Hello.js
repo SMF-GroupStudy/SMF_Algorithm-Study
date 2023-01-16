db94d83433ea18577033540f9a4c2dbc4926a482
csyeob <csyeob@gmail.com>
2023-01-16 22:33:25 +0900

    React Component state 전

--- Members/csyeob/react/voca/src/component/Hello.js
+++ Members/csyeob/react/voca/src/component/Hello.js
@@ -1,33 +1,33 @@
-import World from "./World";
-import styles from "./Hello.module.css";
-// const Hello = function (){
-//     <p>Hello</p>;
-// };
-
-//const Hello = () =>{
-//     <p>Hello</p>;
-//};
-
-// export default Hello;
-
 export default function Hello() {
+    function showName(){
+        console.log("Mike");
+    }
+    function showAge(age){
+        console.log(age);
+    }
+    // function showText(e){
+    //     console.log(e.target.value)
+    //   이렇게 함수로 만들어서 {showText} 해도 되고
+    //  밑에 처럼 해도댐.
+    // }
+     function showText(txt){
+        console.log(txt);
+    }
     return (
-        //  태그는 return 해줘야함.
-        // jsx 는 하나의 태그만 만들수 있음. 그래서 div로 묶어줘야함.
-        // or <> </> 빈태그로 감아줘도댐.
-        // 객체로 작성을 해야함. style = {} 이런식으로 안에 작성
-        // border-right x borderRight 카멜케이스로 작성해야함.
-        <div>
-            <h1 style = {
-                {
-                    color: '#f00',
-                    borderRight : '2px solid #000',
-                    marginBottom : '30px',
-                    opacity : 1,
-                }
-            }>Hello</h1>
-            <World/>
-            <div className={styles.box}>Hello</div>
-        </div>
+     <div>
+        <h1>Hello</h1>
+        <button onClick={showName}>Show name</button>
+        <button onClick={() => {
+            showAge(30)
+        }}>
+        Show age
+        </button>
+        <input type = "text" onChange={(e) =>{
+            const txt = e.target.value;
+            showText(txt);
+            //console.log(e.target.value)
+        }}></input>
+     </div>
     );
 }
+// event
