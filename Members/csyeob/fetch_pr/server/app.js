6fac22a588d0687d56700f1eb0a5a5304489980c
csyeob <csyeob@gmail.com>
2023-01-21 22:43:45 +0900

    api 서버 완성 프론트에서 데이터만 구현해주면 완성

--- Members/csyeob/fetch_pr/server/app.js
+++ Members/csyeob/fetch_pr/server/app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
 let id = 2;
 const todoList = [{
     id: 1,
@@ -12,13 +15,21 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 });
 
-app.get('/api/todo',()=>{
-    return todoList;
+app.get('/api/todo',(req,res)=>{
+    res.json(todoList);
+    //json형태로 보냄.
 });
 
 app.post('/api/todo', (req, res)=>{
-    const {text, done} = req.body
-})
+    const {text, done} = req.body;
+    //body에서 꺼내쓸려면 body parser가 필요함.
+    todoList.push({
+        id:id++,
+        text,
+        done,
+    });
+    return res.send('success');
+});
 
 app.listen(3000, () =>{
     console.log('server start!');
