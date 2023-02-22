1d3d5b589f85f1311504716e7dc1d025052f0c78
csyeob <csyeob@gmail.com>
2023-02-23 00:17:59 +0900

    백준-특정거리의도시찾기(다익스트라)-성공

--- E-book Study/csyeob/Shortest Path/Baekjun_city_find.py
+++ E-book Study/csyeob/Shortest Path/Baekjun_city_find.py
@@ -1,28 +1,35 @@
-## 소개팅 자리를 거쳐가는 것이기 때문에 플로이드워셜을 써야함.
+import heapq
+import sys
+
+input = sys.stdin.readline
 INF = int(1e9)
 
 n, m, k, x = map(int, input().split())
-
-graph = [[INF] * (n+1) for _ in range(n+1)]
-
-for a in range(1, n+1):
-    for b in range(1, n+1):
-        if a == b:
-            graph[a][b] = 0
+graph = [[] for _ in range(n+1)]
+distance = [INF] * (n+1)
 
 for _ in range(m):
-    a, b = map(int,input().split())
-    graph[a][b] = 1
-    graph[b][a] = 1
+    a, b = map(int, input().split())
+    graph[a].append((b, 1))
 
-for t in range(1, n+1):
-    for a in range(1, n+1):
-        for b in range(1, n+1):
-            graph[a][b] = min(graph[a][b], graph[a][t] + graph[t][b])
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+    while q:
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+        for j in graph[now]:
+            cost = dist + j[1]
+            if cost < distance[j[0]]:
+                distance[j[0]] = cost
+                heapq.heappush(q, (cost, j[0]))
 
-result = []
+dijkstra(x)
+result= []
 for i in range(1, n+1):
-    if graph[x][i] == k:
+    if distance[i] == k:
         result.append(i)
         print(i)
 
