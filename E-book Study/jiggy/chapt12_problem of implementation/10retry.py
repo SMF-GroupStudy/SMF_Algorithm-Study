3989db592c051eb93b989324a2e8e19c420caba1
jiggy97 <dkswnsdud159@naver.com>
2023-03-08 15:56:00 +0900

    E-book chapter 12 implementation 10 프로그래머스에서 성공함

--- E-book Study/jiggy/chapt12_problem of implementation/10retry.py
+++ E-book Study/jiggy/chapt12_problem of implementation/10retry.py
@@ -1,61 +1,51 @@
-import copy
-
-
 def check(new_lock, key, n, m, x, y):
-    # 맞는지 판별할 tmp 배열 생성
-    # tmp_lock = new_lock
-    tmp_lock = copy.deepcopy(new_lock)
-
-    # 열쇠의 인덱스값을 하나씩 tmp 필드에 덧샘
+    # 필드에 현재 열쇠의 방향과 위치에 맞게 맞물려 보기
     for i in range(m):
         for j in range(m):
-            tmp_lock[x + i][y + j] += key[i][j]
+            new_lock[x + i][y + j] += key[i][j]
 
+    # 열쇠랑 자물쇠가 딱 맞물린다면 자물쇠가 위치한 부분을 다 더했을 때 n * n이 나옴
     count = 0
-    # 필드 가운데 위치한 자물쇠의 인덱스값 덧셈
     for i in range(n):
         for j in range(n):
-            count += tmp_lock[i + n][j + n]
+            # 맞물렸을 때 자물쇠 부분으 1이라면 (2일 경우 돌기 부분끼리 맞닿은 거임)
+            if new_lock[i + n][j + n] == 1:
+                count += 1
 
-    # 자물쇠와 열쇠를 맞물렸을 때 인덱스값이 모두 1이라면 True return
     if count == n * n:
         return True
     else:
-        return False
+        # 열쇠가 맞지 않았을 경우 다시 열쇠 제거 / 이 부분을 고려하지 못해서 처음에 틀림
+        for i in range(m):
+            for j in range(m):
+                new_lock[x + i][y + j] -= key[i][j]
 
 
 def solution(key, lock):
-    # 열쇠와 자물쇠의 길이
+    # 자물쇠와 열쇠의 길이
     n = len(lock)
     m = len(key)
 
-    # 처음에 2차원 배열을 아래와 같이 만들었더니 하나의 같은 열에 있는 인덱스값들이 같이 바뀜
-    # frame = [[0] * len(lock[0][:]) * 3] * len(lock[:][0]) * 3
-
-    # 열쇠를 이동하며 자물쇠와 맞추기 위한 필드
+    # 자물쇠와 열쇠를 움직이며 맞물릴 수 있는 필드 생성
     new_lock = [[0] * (n * 3) for _ in range(n * 3)]
 
-    # 좌물쇠를 필드 가운데 세팅
+    # 필드에 자물쇠값 입력
     for i in range(n):
         for j in range(n):
             new_lock[i + n][j + n] = lock[i][j]
 
-    # 필드위 열쇠가 (0, 0)에 위치하면 자물쇠와 겹치지 않기 때문에 (1, 1)부터 시작
-    # 즉, x랑 y는 열쇠의 위치
-    for y in range(1, n * 3):
-        # 맞는 방향이 없다면 한칸씩 이동
-        for x in range(1, n * 3):
-            # 열쇠를 4방향으로 돌렸을 때 맞는 방향이 있는지 확인
+    # x, y는 열쇠를 1칸씩 움직여줌 이 때, 맞물리는 지점만 보기 위해 1부터 2*n 까지
+    for y in range(1, n * 2):
+        for x in range(1, n * 2):
+            # 열쇠를 4방향으로 회전해 가며 맞물리며 이동
             for _ in range(4):
+                # 돌기와 홈이 딱 맞물리는지 확인
                 if check(new_lock, key, n, m, x, y):
+                    # 맞물릴 경우 True
                     answer = True
                     return answer
                 # 열쇠 90도 회전
                 key = list(zip(*key[::-1]))
+    # 위 모든 경우의 수를 돌려도 맞지 않다면 불가 False
     answer = False
     return answer
-
-
-a = [[0, 0, 0], [1, 0, 0], [0, 1, 1]]
-b = [[1, 1, 1], [1, 1, 0], [1, 0, 1]]
-print(solution(a, b))
