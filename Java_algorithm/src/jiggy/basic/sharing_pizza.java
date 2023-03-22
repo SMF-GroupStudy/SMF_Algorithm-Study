54c84bc25b173f2f92a0ef718b748440d2417457
jiggy97 <dkswnsdud159@naver.com>
2023-03-22 16:09:50 +0900

    [Programmers] 코딩테스트 입문 - 피자 나눠 먹기(1)

---
+++ Java_algorithm/src/jiggy/basic/sharing_pizza.java
@@ -0,0 +1,17 @@
+package jiggy.basic;
+
+import java.util.Scanner;
+
+public class sharing_pizza {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(solution(n));
+    }
+
+    public static int solution(int n) {
+        if (n % 7 == 0)
+            return n / 7;
+        return (n / 7) + 1;
+    }
+}
