6415861cc1d0417d5558262e25a307b505fd5b59
jiggy97 <dkswnsdud159@naver.com>
2023-04-25 22:32:16 +0900

    [Programmers] 코딩테스트 lv2 - 멀리 뛰기

--- Java_algorithm/src/jiggy/lv2/long_jump.java
+++ Java_algorithm/src/jiggy/lv2/long_jump.java
@@ -21,7 +21,7 @@ public class long_jump {
         dp[2] = 2;
 
         for (int i = 3; i < n + 1; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2] % 1234567;
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
         }
         answer = dp[n];
         return answer;
